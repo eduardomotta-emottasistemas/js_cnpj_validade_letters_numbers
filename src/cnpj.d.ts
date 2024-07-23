f29795316dc0072c932e0a44f874e1b307796d85
Eduardo Motta <eduardo@emotta.com.br>
2024-07-23 12:40:54 -0300

    documentado cnpjGenerator e cnpjFormat

--- src/cnpj.d.ts
+++ src/cnpj.d.ts
@@ -3,5 +3,18 @@
  * @param {string} value - CNPJ number may also contain letters and mask.
  * @returns {boolean} - Returns true for valid CNPJ or false for invalid.
  */
-
 export declare function isValidCnpj(cnpj: string): boolean;
+
+/**
+ *
+ * @param {string} value - optional CNPJ parameter to generate a random CNPJ. If the CNPJ is passed where it is blank, it fills in with a random number or letter.
+ * @returns {string} - Returns random CNPJ.
+ */
+export declare function cnpjGenerator(cnpj: string): boolean;
+
+/**
+ *
+ * @param {string} value - CNPJ number may also contain letters and mask.
+ * @returns {string} - Returns formated CNPJ.
+ */
+export declare function cnpjFormat(cnpj: string): boolean;
